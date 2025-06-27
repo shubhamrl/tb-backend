1c59ee63a0ab2fac106ef0f9e4c57f690dd597e7
shubhamrl <shubhamlasankar10@gmail.com>
2025-06-27 10:50:49 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,19 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// 1. Signup/Register
+// 1. Signup/Register (OTP email send)
 router.post('/signup', authController.register);
 
-// 2. Login
+// 2. Verify OTP (for new user registration)
+router.post('/verify-otp', authController.verifyOtp);
+
+// 3. Login
 router.post('/login', authController.login);
 
-// 3. Forgot Password
+// 4. Forgot Password
 router.post('/forgot-password', authController.forgotPassword);
 
-// 4. Reset Password
+// 5. Reset Password
 router.post('/reset-password/:token', authController.resetPassword);
 
 module.exports = router;
